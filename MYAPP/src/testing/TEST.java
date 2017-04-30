8f2b3034e15d1b8d1e191847dc4b5c590fbeefec
Akansha <Akansha@DESKTOP-T8G3GDV>
2017-04-30 15:56:33 +0530

    tl deleting commit

--- MYAPP/src/testing/TEST.java
+++ MYAPP/src/testing/TEST.java
@@ -3,10 +3,7 @@ package testing;
 public class TEST {
 
 	public static void main(String[] args) {
-		System.out.println("hello");
-		System.out.println("hello");
-		System.out.println("its dev 2");
-		System.out.println("teamleader");
+		System.out.println("tl");
 		
 
 	}
