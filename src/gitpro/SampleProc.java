848ba9014321578c7b9e7fe96678dff8f0a1e7cf
Akansha <Akansha@DESKTOP-T8G3GDV>
2018-06-09 15:47:30 +0530

    commit

--- src/gitpro/SampleProc.java
+++ src/gitpro/SampleProc.java
@@ -1,20 +1,23 @@
 package gitpro;
 
+import java.util.Scanner;
+
 public class SampleProc {
 	
 	
-	 static int a=10;
-	 static int b=20;
+	
 	public static void main(String[] args) {
 		
 		SampleProc sp= new SampleProc();
-		
-		int sum=sp.m1(a, b);
-		System.out.println(sum);
+		Scanner sc = new Scanner(System.in);
+		int a= sc.nextInt();
+		int b =sc.nextInt(); 
+		int sum=sp.m1(a,b);
+	    System.out.println(sum);
 		//System.out.println(sp.b);
 	}
 	
-	public int m1(int a, int  b) {
+	public int m1(int a,int b) {
 		
        int sum =0;
 		sum=a+b;
